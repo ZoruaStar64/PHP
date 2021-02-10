1c8e4d93696dd1203959c7431c413bd3691e72f1
Zorua <sanderb119@gmail.com>
2021-02-10 13:03:37 +0100

    made neccasery pages for H3

--- PHP/H03/h03.php
+++ PHP/H03/h03.php
@@ -1,1 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
 <?php
+echo "<h1>Hoofdstuk 3</h1>";
+
+echo "<p>Welkom bij hoofdstuk 3!</p>";
+
+echo "<p>In hoofdstuk 3 zijn er 8 opdrachten die hieronder te zien zijn</p>";
+
+echo "<p><a href='opdracht1H3.php'>Opdracht 1</a></p>";
+echo "<p><a href='opdracht2H3.php'>Opdracht 2</a></p>";
+echo "<p><a href='opdracht3H3.php'>Opdracht 3</a></p>";
+echo "<p><a href='opdracht4H3.php'>Opdracht 4</a></p>";
+echo "<p><a href='opdracht5H3.php'>Opdracht 5</a></p>";
+echo "<p><a href='opdracht6H3.php'>Opdracht 6</a></p>";
+echo "<p><a href='opdracht7H3.php'>Opdracht 7</a></p>";
+echo "<p><a href='opdracht8H3.php'>Opdracht 8</a></p>";
+
+
+echo "<p><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
