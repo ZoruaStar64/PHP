c9d1d7131e1b9cd2003b729aed9417069c822a8f
Zorua <sanderb119@gmail.com>
2021-02-11 12:58:28 +0100

    Opdracht 2 H3 af en Opdracht 1 H3's array goed gemaakt

--- PHP/H03/opdracht1H3.php
+++ PHP/H03/opdracht1H3.php
@@ -5,6 +5,7 @@
     <title>PHP H3</title>
 </head>
 <body>
+
 <?php
 echo "<h1>Hoofdstuk 3 Opdracht 1</h1>";
 
@@ -14,19 +15,27 @@ echo "<p>In deze opdracht moet ik met zo min mogelijk code 10 afbeeldingen tonen
 
 echo "<p>de plaatjes hieronder zijn deel van de for loop</p>";
 
-for ($teller = 1; $teller < 11; $teller++) {
-    echo "<img src='../../IMG/image" . $teller . ".png' width='150px' height='150px'>";
-    if ($teller === 5) {
+for ($tellerForLoop = 1; $tellerForLoop < 11; $tellerForLoop++) {
+    echo "<img src='../../IMG/image" . $tellerForLoop . ".png' width='150px' height='150px'>";
+    if ($tellerForLoop === 5) {
         echo "<br>";
     }
 }
 echo "<p>de plaatjes hieronder zijn deel van de array</p>";
 
-
 $imageArray = array();
 
-echo $imageArray;
+for ($tellerArray = 1; $tellerArray < 11; $tellerArray++) {
+    $imageArray[] = "<img src='../../IMG/image" . $tellerArray . ".png' width='150px' height='150px'<br>";
 
+    //echo "<script>console.log($teller2)</script>";
+}
+foreach ($imageArray as $img) {
+    echo $img;
+}
+
+
+echo "<br>";
 echo "<p><a href='h03.php'>Terug naar H3</a><br><a class='home' href='../../index.php'>Terug naar home</a></p>";
 
 ?>
--- PHP/H03/opdracht2H3.php
+++ PHP/H03/opdracht2H3.php
@@ -1,1 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
+
 <?php
+echo "<h1>Hoofdstuk 3 Opdracht 2</h1>";
+
+echo "<p>Welkom bij de tweede opdracht van Hoofdstuk 3!</p>";
+
+echo "<p>In deze opdracht moet ik een kerstboom tekenen met twee loops <br>(alleen de form niet een letterlijke kerstboom met bijvoorbeeld een image)</p>";
+
+$teller = 0;
+$star = " * ";
+echo "<div style='text-align: center; width: 150px'>";
+    for ($loopBreak = 0; $loopBreak < 9; $loopBreak++) {
+        $teller++;
+
+    for ($loopSter = 0; $loopSter < 0 + $teller; $loopSter++) {
+        echo $star;
+
+    }
+        echo "<br>";
+}
+echo "</div>";
+
+
+echo "<br>";
+echo "<p><a href='h03.php'>Terug naar H3</a><br><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
