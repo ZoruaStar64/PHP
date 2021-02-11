827b21edf55aed521cfae8f011fa29deaec30c18
Zorua <sanderb119@gmail.com>
2021-02-11 14:15:15 +0100

    got rid of a centered text

--- PHP/H03/opdracht3H3.php
+++ PHP/H03/opdracht3H3.php
@@ -1,1 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
+
 <?php
+echo "<h1>Hoofdstuk 3 Opdracht 3</h1>";
+
+echo "<p>Welkom bij de derde opdracht van Hoofdstuk 3!</p>";
+
+echo "<p>In deze opdracht moet ik een While loop in een For loop veranderen </p>";
+
+
+
+echo "<p><a href='h03.php'>Terug naar H3</a><br><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
