d4108c59da76cd77f4061dfd2119ce55f08b42aa
Zorua <sanderb119@gmail.com>
2021-02-12 12:10:42 +0100

    Opdracht 5 af opdracht 6 bijna af

--- PHP/H03/opdracht5H3.php
+++ PHP/H03/opdracht5H3.php
@@ -1,1 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
+
 <?php
+echo "<h1>Hoofdstuk 3 Opdracht 5</h1>";
+
+echo "<p>Welkom bij de vijfde opdracht van Hoofdstuk 3!</p>";
+
+echo "<p>In deze opdracht moet ik een programma schrijven met minstens 1 variabele<br>deze programma gaat over een busreis die 10 euro kost<br>ouderen boven 65 jaar krijgt 50% korting, hetzelfde geldt voor kinder van 12 jaar of jonger<br>en kinderen onder 3 jaar mogen gratis</p>";
+
+$leeftijd = "80";
+$euro = 10;
+
+if ($leeftijd < 3) {
+    $euro = $euro * 0;
+}
+if ($leeftijd >= 3 && $leeftijd <= 12) {
+    $euro = $euro * 0.5;
+}
+if ($leeftijd > 12 && $leeftijd < 66) {
+    $euro = $euro * 1;
+}
+if ($leeftijd > 65) {
+    $euro = $euro * 0.5;
+}
+
+echo "<p>De leeftijd is op 80 als voorbeeld hieronder</p>";
+echo "<p>In mijn github staat de code waarin de leeftijd/geld uitreken code staat</p>";
+
+echo $euro . " euro";
+
+echo "<br>";
+echo "<p><a href='h03.php'>Terug naar H3</a><br><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
--- PHP/H03/opdracht6H3.php
+++ PHP/H03/opdracht6H3.php
@@ -1,1 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
+
 <?php
+echo "<h1>Hoofdstuk 3 Opdracht 6</h1>";
+
+echo "<p>Welkom bij de zesde opdracht van Hoofdstuk 3!</p>";
+
+echo "<p>In deze opdracht moet ik een programma schrijven<br>die een plaatje weergeeft voor elke 5 zwemmers in een club</p>";
+
+$zwemclub["De spartelkuikens"] = 25;
+$zwemclub["De waterbuffels"] = 32;
+$zwemclub["Plonsmderin"] = 11;
+$zwemclub["Bommetje"] = 23;
+
+
+
+
+
+echo "<div style='border: black 3px solid; width: 300px; height: 96px'>";
+foreach ($zwemclub as $label => $aantal) {
+if ($aantal % 5) {
+    $zwemimage[] = "<img style='width: 20px; height: 20px' src='../../IMG/tada.gif'</img>";
+}
+    echo "<div style='border: black 3px solid; width: 150px; float:left;'>" . $label . "</div>" . "   " . "<div style='border: black 3px solid; width: 30px; float: left'>" . $aantal . "</div>" . $zwemimage  . "<br>";
+}
+echo "</div>";
+
+echo "<br>";
+echo "<p><a href='h03.php'>Terug naar H3</a><br><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
