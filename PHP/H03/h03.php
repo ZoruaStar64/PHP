e72e4ed7b10489eea691c888d9a20f5251a30da7
Zorua <sanderb119@gmail.com>
2021-02-15 13:20:20 +0100

    Opdracht 7 klaar en opdracht 8 verwijdert
    er was geen opdracht 8

--- PHP/H03/h03.php
+++ PHP/H03/h03.php
@@ -10,7 +10,7 @@ echo "<h1>Hoofdstuk 3</h1>";
 
 echo "<p>Welkom bij hoofdstuk 3!</p>";
 
-echo "<p>In hoofdstuk 3 zijn er 8 opdrachten die hieronder te zien zijn</p>";
+echo "<p>In hoofdstuk 3 zijn er 7 opdrachten die hieronder te zien zijn</p>";
 
 echo "<p><a href='opdracht1H3.php'>Opdracht 1</a></p>";
 echo "<p><a href='opdracht2H3.php'>Opdracht 2</a></p>";
@@ -19,8 +19,6 @@ echo "<p><a href='opdracht4H3.php'>Opdracht 4</a></p>";
 echo "<p><a href='opdracht5H3.php'>Opdracht 5</a></p>";
 echo "<p><a href='opdracht6H3.php'>Opdracht 6</a></p>";
 echo "<p><a href='opdracht7H3.php'>Opdracht 7</a></p>";
-echo "<p><a href='opdracht8H3.php'>Opdracht 8</a></p>";
-
 
 echo "<p><a class='home' href='../../index.php'>Terug naar home</a></p>";
 
--- PHP/H03/opdracht7H3.php
+++ PHP/H03/opdracht7H3.php
@@ -1,1 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H3</title>
+</head>
+<body>
+
 <?php
+echo "<h1>Hoofdstuk 3 Opdracht 6</h1>";
+
+echo "<p>Welkom bij de zesde opdracht van Hoofdstuk 3!</p>";
+
+echo "<p>In deze opdracht moet ik een stukje van een programma toevoegen voor een kappersalon</p>";
+
+$kappersagenda["9:15"] = "Mevr.Pietersen";
+$kappersagenda["9:30"] = "Mevr.Willems";
+$kappersagenda["9:45"] = "";
+$kappersagenda["10:00"] = "Paul van den Broek";
+$kappersagenda["10:15"] = "Karel de Meeuw";
+$kappersagenda["10:30"] = "";
+
+
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach($kappersagenda as $tijd => $afspraak) {
+if($afspraak == "") {
+print("<li>".$tijd."</li>") ;
+    }
+}
+print("</ul>");
+
+?>
+</body>
+</html>
+
