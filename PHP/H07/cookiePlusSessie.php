37148c357cc014dc5b4b89b60f1237876391b6cc
Zorua <sanderb119@gmail.com>
2021-03-19 10:29:05 +0100

    H7 klaar
    ik ga nog kijken of het via een database kan (kan zodiezo)

--- PHP/H07/cookiePlusSessie.php
+++ PHP/H07/cookiePlusSessie.php
@@ -1,14 +1,26 @@
+<?php
+session_start();
+
+if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] == "Gebruiker") {
+    echo "<h1>Welkom op deze website " . $_SESSION["user"] ["naam"] . "</h1>";
+}
+else if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] == "Administrator") {
+    echo "<h1>Welkom in de admingedeelte van de website " . $_SESSION["user"] ["naam"] . "</h1>";
+    echo  "<p><a href='adminOnly.php'>u mag nu naar deze pagina</a></p>";
+} else {
+    header('location: opdrachtH7.php');
+}
+
+?>
+
 <head>
     <link rel="stylesheet" href="../../CSS/H7.css">
     <title>PHP H7</title>
 </head>
 
 <body>
-<?php
 
-
-?>
-
+<p><a href="opdrachtH7.php?logout">Klik hier om uit te loggen</a></p>
 </body>
 <?php
 /*$logInOrNo = false;
