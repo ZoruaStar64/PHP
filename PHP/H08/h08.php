90c4f824f5bfd097fa7856fefb8ad20b931e9acf
Zorua <sanderb119@gmail.com>
2021-03-22 15:52:02 +0100

    H8 opdracht 1 af

--- PHP/H08/h08.php
+++ PHP/H08/h08.php
@@ -1,1 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/styles.css">
+    <title>PHP H8</title>
+</head>
+<body>
 <?php
+echo "<h1>Hoofdstuk 8</h1>";
+
+echo "<p>Welkom bij hoofdstuk 8!</p>";
+
+echo "<p>In hoofdstuk 8 heeft 3 opdrachten<br>
+in de eerste moet ik een klasse genaamd Radioprogramma maken met 3 attributen en 2 methoden<br>
+op die pagina moeten de naam van het programma, de omschrijving en de lijst met liedjes staan</p>
+<p>In de tweede moet ik monkeyBusiness.php maken de website heeft 2 afbeeldingen (svg)<br>
+en toont een lijst met namen van verschillende soorten apen<br>
+als je op een apensoort klikt opent het dan een google-zoekpagina met de afbeeldingen van de gekozen aap</p>
+<p>in de laatste opdracht moet ik een website maken voor Mr Wheely's Garagebedrijf met een overzicht van alle occassions op Wheely's website<br>
+Je moet kunnen zoeken op Merk en zoeken op minimale en maximale prijs<br>
+Daarnaast moet de website later ook uitgebreid kunnen worden dus het moet dan Object Geörienteerd gebouwd worden<br>
+(nog een opmerking over de laatste: wie koopt nou een auto van over 100 duizend wanneer er ook een voor bijna 10 duizend is</p>";
+
+echo "<p><a href='opdracht1H8.php'>De eerste opdracht</a></p>";
+
+echo "<p><a href='opdracht2H8.php'>De tweede opdracht</a></p>";
+
+echo "<p><a href='opdracht3H8.php'>De derde opdracht</a></p>";
+
+echo "<p><a class='home' href='../../index.php'>Terug naar home</a></p>";
+
+?>
+</body>
+</html>
