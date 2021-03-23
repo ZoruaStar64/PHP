89ebd926aa62e751838b1651bc6dcd04c5fa5fa7
Zorua <sanderb119@gmail.com>
2021-03-23 16:05:46 +0100

    start monkeybusiness
    had niet echt een goeie dag vandaag
    dat verandert morgen

--- PHP/H08/opdracht2H8.php
+++ PHP/H08/opdracht2H8.php
@@ -1,1 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/monkeyBusiness.css">
+    <title>Monkey Business</title>
+</head>
+<body>
+<h1>vorm van pagina: (alles moet in het midden van de bekijker's scherm zijn)</h1>
+<h2>Monkey Business Logo<br>select your monkey!<br>Image van een aap op een lijn<br>De array van apennamen</h2>
+
 <?php
+
+
+?>
+<p><a class='home' href='../../index.php'>Terug naar home</a></p>
+</body>
+</html>
