8df77cfa2ddeffa8b48ff7a88d1046ff909ee0bd
Zorua <sanderb119@gmail.com>
2021-04-06 16:00:58 +0200

    ver opweg moet alleen ervoor zorgen dat de coins tevoorschijn komen
    en ook nog dat het aantal omlaag kan

--- PHP/EXTRA/wario.js
+++ PHP/EXTRA/wario.js
@@ -4,56 +4,70 @@
    let coin = "../../IMG/warioGame/Coin.png"; //image
    let Lose = "../../IMG/warioGame/Wario-wins.png"; //this was the losing image now i remember
    let Win = "../../IMG/warioGame/Wario-loses.png"; //yes this was the win image
+
    let LoseSound = "../../SOUNDFILES/rotten-day.wav"; //sound
    let WinSound = "../../SOUNDFILES/rotten-day.wav"; //sound
+
    let takebutton = document.querySelector(".takeButton"); //this takes coins
    let restartbutton = document.querySelector(".restartButton"); //this restarts the game
-   let tekstvak = new TextField(); //textarea
+
+   let nummervak = document.querySelector(".numberInput"); //pak dit later nog effe met een queryselector dankje
    let aantal = 23; //ammount of coins
-   let grabber; //this grabs something i think
-   let Errortext = ""; //this text is invisible until an error actually takes place
-   let humantext = "This will tell how many coins you took last round"; //explains itself
-   let computertext = "This will tell how many coins Wario took last round"; //explains itself
-   let winorlose = ""; //think this was for if you win?..
+   let grabber = ""; //this grabs something i think
+
+   let Errortext = document.querySelector(".errorText");
+       Errortext.innerHTML = ""; //this text is invisible until an error actually takes place
+
+   let humantext = document.querySelector(".humanText");
+       humantext.innerHTML = "This will tell how many coins you took last round"; //explains itself
+
+   let computertext = document.querySelector(".computerText");
+       computertext.innerHTML = "This will tell how many coins Wario took last round"; //explains itself
+
+   let winorlose = document.querySelector(".winOrLose");
+       winorlose.innerHTML = ""; //changes depending on wether you win or lose
+
+   let totalOfCoins = document.querySelector(".totalOfCoins")
+       totalOfCoins.innerHTML = "The total of remaining coins = " + aantal;
+
    let Botchoices = [3]; //an array with choices the bot can have
+
+   let coins = document.querySelector(".coins")
+       coins.innerHTML = [];
    let converter; //this converts the actual number of coins with the ammount of images needed on screen
    let PlayerTurn = true; //this decides wether or not it's your turn
 
-        takebutton.addEventListener("click", Subtractionlistener);
-        tekstvak.addEventListener(Subtractionlistener);
-        restartbutton.addEventListener("click", Resetlistener);
+        takebutton.addEventListener('click', Subtractionlistener);
+        nummervak.addEventListener('click' ,Subtractionlistener);
+        restartbutton.addEventListener('click', Resetlistener);
 
-        add(tekstvak);
+
 
         //Botchoices = new String[3]; (not sure if this is needed)
-        restartbutton.style.color = "red"; //this has a red color
 
-    public void paint(Graphics g) { //alright just gotta check my js work to see how i connect things from html to js
+
+    //alright just gotta check my js work to see how i connect things from html to js
     //LoseSound.play();
-    g.drawString("" + humantext, 105,55);
-    g.drawString("" + Errortext, 105,55);
-    g.drawString("" + computertext, 105, 70);
-    g.drawString("" + winorlose, 105, 300);
-    g.drawString("The rules are simple: don't be the one that grabs the last coin!", 50,40);
-    g.drawString("The total of remaining coins = " + aantal, 105, 85);
+
+
     let j = 1;
     let k = 1;
     for ( let i = 1; i <= aantal; i++) {
     k++;
-    g.drawImage(coin, (40 * k + 20), (50 * j + 50 ), 40, 50, this);
+    coins = coins + coin;
     if (i % 5 === 0) {
     j++;
     k = 1;
     }
 }
 g.drawImage(selectedimage, 100,100,150,150, this); //surprises me that drawimage is an actual thing in js
-}
+
 
 
 function Subtractionlistener() { //ah yes actionlisteners... lemme figure out the way to do them in js...
 
     // De menselijke speler's input
-    grabber = tekstvak;
+    grabber = nummervak;
     converter = parseInt(grabber);
     for (let fakeloop = 0; fakeloop === 0; fakeloop++) { //think this may be useless idk tho
     PlayerTurn = true;
@@ -67,7 +81,7 @@ if (converter === 1 || converter === 2 || converter === 3) {
     aantal = aantal - converter;
     humantext = "You took " + converter + " coin(s)";
     Errortext = "";
-    tekstvak = "";
+    nummervak = "";
 }
 
 else {
@@ -133,7 +147,6 @@ if (aantal < 0) {
     aantal = 0;
     break;
 }
-repaint(); //i think repaint() isn't needed?.. i'll have to look at my JS work quickly
 
 if (aantal === 0 && PlayerTurn) {
     WinSound.play();
@@ -151,5 +164,6 @@ function Resetlistener() { //probably needs an actionlistener idk
     humantext = "";
     Errortext = "";
     winorlose = "";
+    nummervak = "";
     selectedimage = null;
 }
