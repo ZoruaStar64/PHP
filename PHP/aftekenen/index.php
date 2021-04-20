79297c36c8990df26a8eabfacfc34ae11d05a668
Zorua <sanderb119@gmail.com>
2021-04-20 13:50:42 +0200

    aftekenen compleet en styles laatste keer aangepast

---
+++ PHP/aftekenen/index.php
@@ -0,0 +1,14 @@
+<?php
+$variable1 = 10;
+$variable2 = 10;
+
+if ($variable1 > $variable2) {
+    echo $variable1 . " is groter";
+}
+else if ($variable2 > $variable1) {
+    echo $variable2 . " is groter";
+}
+else {
+    echo $variable1 . " is gelijk aan " . $variable2;
+}
+
